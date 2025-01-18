675ce472ab6f5d46c603d885d38b0b0fa8d00e81
NahiMohammed <mohammednahi560@gmail.com>
2025-01-18 16:47:31 +0100

    update

--- 1A/S6/tob/projet court/projet-court/src/allumettes/OperationInterditeException.java
+++
@@ -1,9 +0,0 @@
-package allumettes;
-public class OperationInterditeException extends RuntimeException {
-        /**La levée d'une exception avec un message en paramètre.
-        * @param message
-        */
-		public OperationInterditeException(String message) {
-			super(message);
-			}
-	}
--- 1A/S6/tob/projet long/src/Bibliotheque/Main.java
+++
@@ -1,8 +0,0 @@
-package Bibliotheque;
-
-public class Main {
-    public static void main(String[] args) {
-
-                new FenetrePrincipale();     
-    }
-}
